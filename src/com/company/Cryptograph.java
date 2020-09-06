4e8190216b6accaa0606513043240ecbf54b9be7
VorXal <alexey32101231@gmail.com>
2020-09-06 15:29:18 +0400

    Added matrix for encryption

--- src/com/company/Cryptograph.java
+++ src/com/company/Cryptograph.java
@@ -1,9 +1,62 @@
 package com.company;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Cryptograph {
     private String mainString;
-    public Cryptograph(String input){
+    private String key;
+    public Cryptograph(String input, String key){
         this.mainString = input;
+
+        char[] chars = key.toCharArray();
+        Set<Character> charSet = new LinkedHashSet<Character>();
+        for (char c : chars) {
+            charSet.add(c);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Character character : charSet) {
+            sb.append(character);
+        }
+
+        this.key = sb.toString().toUpperCase();
+    }
+
+    String alphabet = "abcdefghiklmnopqrstuvwxyz".toUpperCase();
+
+    public char[][] matrix = new char[5][5];
+
+    public char[][] getMatrix(){
+        for (int k = 0, i = 0, j = 0; k < key.length(); k++){
+            if(i > 4){
+                j++;
+                i = 0;
+            }
+            matrix[j][i] = Character.toUpperCase(key.charAt(k));
+            i++;
+        }
+
+
+        for (char ch: this.key.toCharArray()) {
+            if(alphabet.indexOf(ch) != -1){
+                alphabet = alphabet.substring(0, alphabet.indexOf(ch)) + alphabet.substring(alphabet.indexOf(ch) + 1);
+            }
+        }
+
+        for (char ch : this.alphabet.toCharArray()) {
+            outerloop:{
+                for(int i = 0; i < 5; i++){
+                    for (int j = 0; j < 5; j++){
+                        if(matrix[i][j] == '\u0000'){
+                            matrix[i][j] = ch;
+                            break outerloop;
+                        }
+                    }
+                }
+            }
+        }
+        return matrix;
     }
 
     public String[] toBigram(){
@@ -15,7 +68,7 @@ public class Cryptograph {
         }
 
         for (int i = 0, j = 0; i < mainString.length(); i+=2, j++){
-            arrayChars[j] = String.valueOf(mainString.charAt(i)) + mainString.charAt(i + 1);
+            arrayChars[j] = String.valueOf(mainString.charAt(i)).toUpperCase() + String.valueOf(mainString.charAt(i + 1)).toUpperCase();
         }
 
         return arrayChars;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Cryptograph cr = new Cryptograph("abcde");
+        Cryptograph cr = new Cryptograph("Hello","helicopo");
         String[] chars = cr.toBigram();
+        char[][] key = cr.getMatrix();
         for (String i : chars) {
             System.out.println(i);
+        }
+
+        for (char[] value : key) {
+            System.out.println();
+            for (char c : value) {
+                System.out.print(c + "\t");
+            }
         }
     }
 }
